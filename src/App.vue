a77b4f5619d7051002aea1e1bd82d32f8acc2250
Jorbbey <jorbbey.gmail.com>
2022-11-05 02:18:29 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserGen />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserGen from './components/UserGen.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserGen
   }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  html,body{
+      font-family: Arial, Helvetica, sans-serif;
+  }
+
+  #app {
+    width: 400px;
+    height: 100vh;
+    margin: auto;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1, h3 {
+    margin-bottom: 1rem;
+    font-weight: normal;
+  }
+
+  img{
+    border-radius: 50%;
+    border: 5px #333 solid;
+    margin-bottom: 1rem;
+  }
+
+  .male{
+    border-color: green;
+    background: green;
+  }
+
+  .female{
+    border-color: deeppink;
+    background: deeppink;
+  }
+
+  button{
+    cursor: pointer;
+    display: inline-block;
+    background: #333;
+    color: white;
+    font-size: 18px;
+    border: 0;
+    padding: 1rem 1.5rem;
+    border-radius: 10px;
+    transition: .5s;
+  }
+  button:focus{
+    outline: none;
+  }
+
+  button:hover{
+    transform: scale(0.98);
+  }
 </style>
---
+++ src/components/UserGen.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+<img 
+  :src="picture" 
+  :alt=" ` ${firstName} ${lastName} ` " 
+   :class="gender"/>
+  <h1> {{ firstName }} {{ lastName }}</h1>
+  <h3>Email: {{ email }}</h3>
+  <button @click="getUser()" :class="gender">Get User</button> 
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return{
+        firstName: 'Smith',
+        lastName: 'Johnson',
+        email: 'smithjohn@gmail.com',
+        gender: 'male',
+        picture: 'https://randomuser.me/api/portraits/men/20.jpg'
+      }
+    },
+    methods: {
+      async getUser() {
+        const res = await fetch('https://randomuser.me/api');
+        const { results } = await res.json();
+        this.firstName = results[0].name.first
+        this.lastName = results[0].name.last
+        this.email = results[0].email
+        this.gender = results[0].gender
+        this.picture = results[0].picture.large
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
