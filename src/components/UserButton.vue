6e3984dd5db5648068e9488722aee0fe27752adc
Jorbbey <jorbbey.gmail.com>
2022-12-27 03:21:14 +0100

    commit

---
+++ src/components/UserButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="$emit('get-user')">{{ text }}</button> 
+</template>
+
+<script>
+export default {
+     name: 'UserButton',
+          methods: {
+      getImage() {
+          this.picture = this.results[0].picture.extraLarge
+      }
+          },
+          props: {
+               text: String
+          },
+          emits:['get-user']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/UserGen.vue
+++ src/components/UserGen.vue
@@ -1,40 +1,44 @@
 <template>
-    <div>
-<img 
-  :src="picture" 
-  :alt=" ` ${firstName} ${lastName} ` " 
-   :class="gender"/>
-  <h1> {{ firstName }} {{ lastName }}</h1>
-  <h3>Email: {{ email }}</h3>
-  <button @click="getUser()" :class="gender">Get User</button> 
-    </div>
+  <div>
+    <img
+      :src="picture"
+      :alt="` ${firstName} ${lastName} `"
+      :class="gender"
+      @click="getImage"
+    />
+    <h1>{{ firstName }} {{ lastName }}</h1>
+    <h3>Email: {{ email }}</h3>
+    <UserButton @get-user="getUser" :class="gender" text="Get User" />
+  </div>
 </template>
 
 <script>
+import UserButton from './UserButton.vue'
 export default {
-    data() {
-      return{
-        firstName: 'Smith',
-        lastName: 'Johnson',
-        email: 'smithjohn@gmail.com',
-        gender: 'male',
-        picture: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    },
-    methods: {
-      async getUser() {
-        const res = await fetch('https://randomuser.me/api');
-        const { results } = await res.json();
-        this.firstName = results[0].name.first
-        this.lastName = results[0].name.last
-        this.email = results[0].email
-        this.gender = results[0].gender
-        this.picture = results[0].picture.large
-      }
+  data() {
+    return {
+      firstName: 'Smith',
+      lastName: 'Johnson',
+      email: 'smithjohn@gmail.com',
+      gender: 'male',
+      picture: 'https://randomuser.me/api/portraits/men/20.jpg',
     }
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api')
+      const { results } = await res.json()
+      this.firstName = results[0].name.first
+      this.lastName = results[0].name.last
+      this.email = results[0].email
+      this.gender = results[0].gender
+      this.picture = results[0].picture.large
+    },
+  },
+  components: {
+    UserButton,
+  },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
